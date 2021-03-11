b354db06932c4e0f6a113fdbe72931e2f1d9cbae
Juan Salomón Sains Hernández <salo_zizu@hotmail.com>
2021-03-10 18:49:13 -0600

    se modifico el modelo de los datosNodemcu

--- models/datosNodemcu.go
+++ models/datosNodemcu.go
@@ -4,10 +4,14 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Datos struct {
 	ID         	primitive.ObjectID 	`bson:"_id" json:"id"`
-	B1   		float64     `bson:"B1" json:"B1"`
-	B2   		float64     `bson:"B2" json:"B2"`
-	B3   		float64     `bson:"B3" json:"B3"`
-	B4   		float64     `bson:"B4" json:"B4"`
+	B1   		string     `bson:"B1" json:"B1"`
+	Est1		string		`bson:"est1" json:"est1"`
+	B2   		string     `bson:"B2" json:"B2"`
+	Est2		string		`bson:"est2" json:"est2"`
+	B3   		string     `bson:"B3" json:"B3"`
+	Est3		string		`bson:"est3" json:"est3"`
+	B4   		string     `bson:"B4" json:"B4"`
+	Est4		string		`bson:"est4" json:"est4"`
 	Fecha		string		`bson:"fecha" json:"fecha"`
 	Hora		string		`bson:"hora" json:"hora"`
 	MacNodemcu	string		`bson:"macNodemcu" json:"macNodemcu"`
