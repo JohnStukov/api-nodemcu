6514810178b9824f7bc6ff75949913674398295c
Ignis Divine <salo_zizu@hotmail.com>
2021-05-21 14:58:14 -0500

    se añadieron los sensores

--- models/datosNodemcu.go
+++ models/datosNodemcu.go
@@ -3,10 +3,12 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Datos struct {
-	ID         	primitive.ObjectID 	`bson:"_id" json:"id"`
-	B   		string     `bson:"B" json:"B"`
-	Est		string		`bson:"est" json:"est"`
-	Fecha		string		`bson:"fecha" json:"fecha"`
-	Hora		string		`bson:"hora" json:"hora"`
-	MacNodemcu	string		`bson:"macNodemcu" json:"macNodemcu"`
+	ID          primitive.ObjectID `bson:"_id" json:"id"`
+	Movimiento  string             `bson:"movimiento" json:"movimiento"`
+	Sonico      string             `bson:"sonico" json:"sonico"`
+	Temperatura string             `bson:"temperatura" json:"temperatura"`
+	Humedad     string             `bson:"humedad" json:"humedad"`
+	Fecha       string             `bson:"fecha" json:"fecha"`
+	Hora        string             `bson:"hora" json:"hora"`
+	MacNodemcu  string             `bson:"macNodemcu" json:"macNodemcu"`
 }
