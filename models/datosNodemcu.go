becf129b601ab3672a2779d049036364907ae2fa
Ignis Divine <salo_zizu@hotmail.com>
2021-05-24 20:33:01 -0500

    modificacion al modelo datosNodemcu.go

--- models/datosNodemcu.go
+++ models/datosNodemcu.go
@@ -5,9 +5,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Datos struct {
 	ID          primitive.ObjectID `bson:"_id" json:"id"`
 	Movimiento  string             `bson:"movimiento" json:"movimiento"`
-	Sonico      string             `bson:"sonico" json:"sonico"`
-	Temperatura string             `bson:"temperatura" json:"temperatura"`
-	Humedad     string             `bson:"humedad" json:"humedad"`
+	Sonico      float32            `bson:"sonico" json:"sonico"`
+	Temperatura float32            `bson:"temperatura" json:"temperatura"`
+	Humedad     float32            `bson:"humedad" json:"humedad"`
 	Fecha       string             `bson:"fecha" json:"fecha"`
 	Hora        string             `bson:"hora" json:"hora"`
 	MacNodemcu  string             `bson:"macNodemcu" json:"macNodemcu"`
