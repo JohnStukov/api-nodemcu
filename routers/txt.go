e011ecdb4f7caf2ed6f84a7d27a95d5ad07881cc
Juan Salomón Sains Hernández <55299785+Ignis-Divine@users.noreply.github.com>
2021-07-27 21:03:33 -0500

    Update txt.go

--- routers/txt.go
+++ routers/txt.go
@@ -9,14 +9,14 @@ import (
 
 func Txt(dispositivo string, tipo string) {
 	// Set initial variables
-	accountSid := "ACfe54870522cae6697bd9e6337bfa7cbb"
-	authToken := "edeae3f09b4a806e28765cb44267f8f4"
+	accountSid := ""
+	authToken := ""
 	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
 
 	// Build out the data for our message
 	v := url.Values{}
-	v.Set("To","+524271417372")
-	v.Set("From","+12056977554")
+	v.Set("To","")
+	v.Set("From","")
 	v.Set("Body","Se disparo la alarma " + tipo + "! Dispositivo: " + dispositivo)
 	rb := *strings.NewReader(v.Encode())
 
